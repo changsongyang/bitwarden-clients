1c6ec5508960c2c673da3c3a4a08dbbcc7c1048f
Jonathan Prusik <jprusik@classynemesis.com>
2024-12-13 11:06:17 -0500

    add BlockBrowserInjectionsByDomain feature flag and put feature behind it

--- libs/angular/src/services/jslib-services.module.ts
+++ libs/angular/src/services/jslib-services.module.ts
@@ -1246,7 +1246,7 @@ const safeProviders: SafeProvider[] = [
   safeProvider({
     provide: DomainSettingsService,
     useClass: DefaultDomainSettingsService,
-    deps: [StateProvider],
+    deps: [StateProvider, ConfigService],
   }),
   safeProvider({
     provide: BiometricStateService,
