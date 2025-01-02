34113b0c4636358c1fcd77a9f94530b91886931b
Bernd Schoolmann <mail@quexten.com>
2025-01-02 11:36:22 +0100

    Prevent emergency access dialog spoofing

--- apps/web/src/app/auth/emergency-access/services/emergency-access.service.ts
+++ apps/web/src/app/auth/emergency-access/services/emergency-access.service.ts
@@ -154,13 +154,11 @@ export class EmergencyAccessService
    * @param id emergency access id
    * @param token secret token provided in email
    */
-  async confirm(id: string, granteeId: string) {
+  async confirm(id: string, granteeId: string, publicKey: Uint8Array): Promise<void> {
     const userKey = await this.keyService.getUserKey();
     if (!userKey) {
       throw new Error("No user key found");
     }
-    const publicKeyResponse = await this.apiService.getUserPublicKey(granteeId);
-    const publicKey = Utils.fromB64ToArray(publicKeyResponse.publicKey);
 
     try {
       this.logService.debug(
