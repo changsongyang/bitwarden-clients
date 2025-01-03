bitwarden/clients

PoC implementation

apps/browser/src/autofill/notification/bar.ts
@@ -1,3 +1,5 @@
+import { render } from "lit";
+
 import { ThemeType } from "@bitwarden/common/platform/enums";
 import { ConsoleLogService } from "@bitwarden/common/platform/services/console-log.service";
 import type { FolderView } from "@bitwarden/common/vault/models/view/folder.view";
@@ -12,8 +14,13 @@ import {
   NotificationBarWindowMessage,
   NotificationBarIframeInitData,
 } from "./abstractions/notification-bar";
+import { OuterWrapper } from "./components/outer-wrapper";
 
-require("./bar.scss");
+const useComponentBar = true;
+
+if (!useComponentBar) {
+  require("./bar.scss");
+}
 
 const logService = new ConsoleLogService(false);
 let notificationBarIframeInitData: NotificationBarIframeInitData = {};
@@ -36,12 +43,7 @@ function initNotificationBar(message: NotificationBarWindowMessage) {
   }
 
   notificationBarIframeInitData = initData;
-  const { isVaultLocked } = notificationBarIframeInitData;
-  setNotificationBarTheme();
-
-  (document.getElementById("logo") as HTMLImageElement).src = isVaultLocked
-    ? chrome.runtime.getURL("images/icon38_locked.png")
-    : chrome.runtime.getURL("images/icon38.png");
+  const { isVaultLocked, theme } = notificationBarIframeInitData;
 
   const i18n = {
     appName: chrome.i18n.getMessage("appName"),
@@ -61,6 +63,30 @@ function initNotificationBar(message: NotificationBarWindowMessage) {
     lpCancelFilelessImport: chrome.i18n.getMessage("lpCancelFilelessImport"),
     startFilelessImport: chrome.i18n.getMessage("startFilelessImport"),
   };
+
+  if (useComponentBar) {
+    document.body.innerHTML = "";
+    const themeType = getTheme(globalThis, theme);
+
+    // There are other possible passed theme values, but for now, resolve to dark or light
+    const resolvedTheme = themeType === ThemeType.Dark ? ThemeType.Dark : ThemeType.Light;
+
+    return render(
+      OuterWrapper({
+        ...notificationBarIframeInitData,
+        theme: resolvedTheme,
+        handleCloseNotification,
+        i18n,
+      }),
+      document.body,
+    );
+  }
+
+  setNotificationBarTheme();
+
+  (document.getElementById("logo") as HTMLImageElement).src = isVaultLocked
+    ? chrome.runtime.getURL("images/icon38_locked.png")
+    : chrome.runtime.getURL("images/icon38.png");
 
   setupLogoLink(i18n);
 
@@ -124,7 +150,7 @@ function initNotificationBar(message: NotificationBarWindowMessage) {
   closeButton.title = i18n.close;
 
   const notificationType = initData.type;
-  if (initData.type === "add") {
+  if (notificationType === "add") {
     handleTypeAdd();
   } else if (notificationType === "change") {
     handleTypeChange();
@@ -134,15 +160,17 @@ function initNotificationBar(message: NotificationBarWindowMessage) {
     handleTypeFilelessImport();
   }
 
-  closeButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    sendPlatformMessage({
-      command: "bgCloseNotificationBar",
-    });
-  });
+  closeButton.addEventListener("click", handleCloseNotification);
 
   globalThis.addEventListener("resize", adjustHeight);
   adjustHeight();
+}
+
+function handleCloseNotification(e: Event) {
+  e.preventDefault();
+  sendPlatformMessage({
+    command: "bgCloseNotificationBar",
+  });
 }
 
 function handleTypeAdd() {
@@ -390,13 +418,18 @@ function setupLogoLink(i18n: Record<string, string>) {
   sendPlatformMessage({ command: "getWebVaultUrlForNotification" }, setWebVaultUrlLink);
 }
 
-function setNotificationBarTheme() {
-  let theme = notificationBarIframeInitData.theme;
+function getTheme(globalThis: any, theme: NotificationBarIframeInitData["theme"]) {
   if (theme === ThemeType.System) {
-    theme = globalThis.matchMedia("(prefers-color-scheme: dark)").matches
+    return globalThis.matchMedia("(prefers-color-scheme: dark)").matches
       ? ThemeType.Dark
       : ThemeType.Light;
   }
+
+  return theme;
+}
+
+function setNotificationBarTheme() {
+  const theme = getTheme(globalThis, notificationBarIframeInitData.theme);
 
   document.documentElement.classList.add(`theme_${theme}`);
 

changsongyang/bitwarden-clients

apps/browser/src/autofill/notification/bar.ts
@@ -1,3 +1,5 @@
+import { render } from "lit";
+
 // FIXME: Update this file to be type safe and remove this and next line
 // @ts-strict-ignore
 import { ThemeTypes } from "@bitwarden/common/platform/enums";
@@ -14,8 +16,13 @@ import {
   NotificationBarWindowMessage,
   NotificationBarIframeInitData,
 } from "./abstractions/notification-bar";
+import { OuterWrapper } from "./components/outer-wrapper";
 
-require("./bar.scss");
+const useComponentBar = true;
+
+if (!useComponentBar) {
+  require("./bar.scss");
+}
 
 const logService = new ConsoleLogService(false);
 let notificationBarIframeInitData: NotificationBarIframeInitData = {};
@@ -38,12 +45,7 @@ function initNotificationBar(message: NotificationBarWindowMessage) {
   }
 
   notificationBarIframeInitData = initData;
-  const { isVaultLocked } = notificationBarIframeInitData;
-  setNotificationBarTheme();
-
-  (document.getElementById("logo") as HTMLImageElement).src = isVaultLocked
-    ? chrome.runtime.getURL("images/icon38_locked.png")
-    : chrome.runtime.getURL("images/icon38.png");
+  const { isVaultLocked, theme } = notificationBarIframeInitData;
 
   const i18n = {
     appName: chrome.i18n.getMessage("appName"),
@@ -63,6 +65,30 @@ function initNotificationBar(message: NotificationBarWindowMessage) {
     lpCancelFilelessImport: chrome.i18n.getMessage("lpCancelFilelessImport"),
     startFilelessImport: chrome.i18n.getMessage("startFilelessImport"),
   };
+
+  if (useComponentBar) {
+    document.body.innerHTML = "";
+    const themeType = getTheme(globalThis, theme);
+
+    // There are other possible passed theme values, but for now, resolve to dark or light
+    const resolvedTheme = themeType === ThemeTypes.Dark ? ThemeTypes.Dark : ThemeTypes.Light;
+
+    return render(
+      OuterWrapper({
+        ...notificationBarIframeInitData,
+        theme: resolvedTheme,
+        handleCloseNotification,
+        i18n,
+      }),
+      document.body,
+    );
+  }
+
+  setNotificationBarTheme();
+
+  (document.getElementById("logo") as HTMLImageElement).src = isVaultLocked
+    ? chrome.runtime.getURL("images/icon38_locked.png")
+    : chrome.runtime.getURL("images/icon38.png");
 
   setupLogoLink(i18n);
 
@@ -126,7 +152,7 @@ function initNotificationBar(message: NotificationBarWindowMessage) {
   closeButton.title = i18n.close;
 
   const notificationType = initData.type;
-  if (initData.type === "add") {
+  if (notificationType === "add") {
     handleTypeAdd();
   } else if (notificationType === "change") {
     handleTypeChange();
@@ -136,15 +162,17 @@ function initNotificationBar(message: NotificationBarWindowMessage) {
     handleTypeFilelessImport();
   }
 
-  closeButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    sendPlatformMessage({
-      command: "bgCloseNotificationBar",
-    });
-  });
+  closeButton.addEventListener("click", handleCloseNotification);
 
   globalThis.addEventListener("resize", adjustHeight);
   adjustHeight();
+}
+
+function handleCloseNotification(e: Event) {
+  e.preventDefault();
+  sendPlatformMessage({
+    command: "bgCloseNotificationBar",
+  });
 }
 
 function handleTypeAdd() {
@@ -392,13 +420,18 @@ function setupLogoLink(i18n: Record<string, string>) {
   sendPlatformMessage({ command: "getWebVaultUrlForNotification" }, setWebVaultUrlLink);
 }
 
-function setNotificationBarTheme() {
-  let theme = notificationBarIframeInitData.theme;
+function getTheme(globalThis: any, theme: NotificationBarIframeInitData["theme"]) {
   if (theme === ThemeTypes.System) {
-    theme = globalThis.matchMedia("(prefers-color-scheme: dark)").matches
+    return globalThis.matchMedia("(prefers-color-scheme: dark)").matches
       ? ThemeTypes.Dark
       : ThemeTypes.Light;
   }
+
+  return theme;
+}
+
+function setNotificationBarTheme() {
+  const theme = getTheme(globalThis, notificationBarIframeInitData.theme);
 
   document.documentElement.classList.add(`theme_${theme}`);
 